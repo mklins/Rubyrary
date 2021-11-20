114456480755a30f38719ff9775b5592f6d192a0
mklits <nikolai.litynskyi@gmail.com>
2021-11-20 05:57:22 +0200

    Changed default user type when adding user_id to books in migration

--- db/migrate/20211111105336_add_user_id_to_books.rb
+++ db/migrate/20211111105336_add_user_id_to_books.rb
@@ -2,6 +2,6 @@
 
 class AddUserIdToBooks < ActiveRecord::Migration[6.1]
   def change
-    add_reference :books, :user, null: false, foreign_key: true, default: User.first.id
+    add_reference :books, :user, null: false, foreign_key: true, default: 0
   end
 end
