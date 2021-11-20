d9fccba7e7156331951dad83d3cf42412c52aae3
mklits <nikolai.litynskyi@gmail.com>
2021-11-20 06:37:27 +0200

    Added to seed book groups not from faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do
-  title = Faker::Book.genre
-  Group.create title: title
+# 20.times do
+#   title = Faker::Book.genre
+#   Group.create title: title
+# end
+
+groups = ["Fantasy", "Science Fiction", "Action & Adventure",
+        "Crime/Detective", "Horror", "Historical fiction", "Realistic fiction",
+        "Metafiction", "Fairy tale", "Folklore", "Legend", "Classic", "Humor",
+        "Narrative nonfiction", "Thriller & Suspense", "Romance",
+        "Graphic Novel", "History", "Memoir & Autobiography", "Humanities & Social Sciences"]
+
+for groups do |group|
+    Group.create title: group
 end
